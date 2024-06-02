f1f2bacf23500a019755132313285d8c19977ca9
Carlos Lopez <carloslopezr29@gmail.com>
2024-06-02 13:35:40 -0400

    First commit

---
+++ ClassViews/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.views import View
+
+class HomeView(View):
+    def get(self, request):
+        return HttpResponse('Wlelcome')
+
+class AboutView(View):
+    def get(self, request):
+        return render(request, 'about.html')
